2f46ac8ecb22e62a06744f9e3537e5013ee0221b
dongw00 <albadw0428@gmail.com>
2019-05-15 19:54:17 +0900

    done, doing

--- src/main/java/com/grep/project/intern/controller/TodoController.java
+++ src/main/java/com/grep/project/intern/controller/TodoController.java
@@ -11,13 +11,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collection;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/todo")
 @Api(value = "Todo List")
 public class TodoController {
     private final Logger log = LoggerFactory.getLogger(TodoController.class);
@@ -28,13 +27,13 @@ public class TodoController {
     }
 
     @ApiOperation(value = "모든 todo를 GET")
-    @GetMapping("/todo")
+    @GetMapping
     Collection<TodoList> groups() {
         return todoListRepository.findAll();
     }
 
     @ApiOperation(value = "해당 id todo를 GET")
-    @GetMapping("/todo/{id}")
+    @GetMapping("/{id}")
     ResponseEntity<?> getTodo(@PathVariable Long id) {
         Optional<TodoList> todoList = todoListRepository.findById(id);
         return todoList.map(res -> ResponseEntity.ok().body(res))
@@ -42,16 +41,15 @@ public class TodoController {
     }
 
     @ApiOperation(value = "todo POST")
-    @PostMapping("/todo")
+    @PostMapping
     ResponseEntity<TodoList> createGroup(@Valid @RequestBody TodoList todoList) throws URISyntaxException {
         log.info("todo를 생성했습니다 : {}", todoList);
         TodoList result = todoListRepository.save(todoList);
-        return ResponseEntity.created(new URI("/api/todo/" + result.getId()))
-                .body(result);
+        return ResponseEntity.ok().body(result);
     }
 
     @ApiOperation(value = "todo 수정")
-    @PutMapping("/todo/{id}")
+    @PutMapping("/{id}")
     ResponseEntity<TodoList> updateGroup(@Valid @RequestBody TodoList todoList) {
         log.info("해당 todo를 수정하였습니다 :  {}", todoList);
         TodoList result = todoListRepository.save(todoList);
@@ -59,7 +57,7 @@ public class TodoController {
     }
 
     @ApiOperation(value = "todo 삭제")
-    @DeleteMapping("/todo/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<?> deleteGroup(@PathVariable Long id) {
         log.info("해당 todo를 삭제하였습니다 : {}", id);
         todoListRepository.deleteById(id);
--- src/main/java/com/grep/project/intern/model/TodoList.java
+++ src/main/java/com/grep/project/intern/model/TodoList.java
@@ -1,8 +1,11 @@
 package com.grep.project.intern.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
 import java.util.Date;
 
 @Data
@@ -24,14 +27,14 @@ public class TodoList {
     @NonNull
     private int sequence;
 
-    @NonNull
-    private String dueDate;
+    @DateTimeFormat(pattern = "yyyy-MM-dd'T'HH:mm")
+    private ZonedDateTime dueDate;
 
     @NonNull
     private boolean done;
 
     @Builder
-    public TodoList(String title, String content, int sequence, String dueDate) {
+    public TodoList(String title, String content, int sequence, ZonedDateTime dueDate) {
         this.title = title;
         this.content = content;
         this.sequence = sequence;
@@ -41,6 +44,6 @@ public class TodoList {
 
     @Override
     public String toString() {
-        return "ID = " + id + ", title = " + title;
+        return "ID = " + id + ", title = " + title + ", dueDate = " + dueDate;
     }
 }
